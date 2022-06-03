af61e0c06b721b8046963585563d6f398f89b06f
Timothy Coleman <timhc22@gmail.com>
2022-06-03 20:55:59 +0100

    stop panning and add loading 3d view

--- src/App.tsx
+++ src/App.tsx
@@ -23,13 +23,13 @@ function App() {
         <DefaultXRControllers />
 
         {/*lock zoom to keep dolls house view. Can use minPolarAngle={Math.PI/2.1} maxPolarAngle={Math.PI/2.1} to lock rotation */}
-        <OrbitControls enableZoom={false} />
+        <OrbitControls enableZoom={false} enablePan={false} />
 
         <ambientLight/>
         <pointLight intensity={3} position={[0, 0, 0]}/>
         <PerspectiveCamera position={[5,5,5]} makeDefault/>
 
-        <Suspense fallback={<Html className="white">loading..</Html>}>
+        <Suspense fallback={<Html className="white">loading 3d view..</Html>}>
           <Space />
         </Suspense>
       </VRCanvas>
