a629422191aa4c09bd8dfd7fc4af3f123aa550e9
Timothy Coleman <timhc22@gmail.com>
2022-06-15 19:13:20 +0100

    Add drawer

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,37 @@ import NavBar from "./components/NavBar";
 import Space from "./components/Space";
 import {DefaultXRControllers, useXR, VRCanvas} from '@react-three/xr';
 import {Html, OrbitControls, PerspectiveCamera} from "@react-three/drei";
-import {CameraAltOutlined, ChevronLeft, ChevronRight, InfoOutlined} from "@mui/icons-material";
+import {CameraAltOutlined, ChevronLeft, ChevronRight, InfoOutlined, Menu} from "@mui/icons-material";
 import PhotoViewer from "./components/PhotoViewer";
 import InfoModal from "./components/InfoModal";
+import LeftSideDrawer from "./components/LeftSideDrawer";
 
 function App() {
   const { player } = useXR();
-  const [showImages, setShowImages] = useState(true);
+  const [showImages, setShowImages] = useState(false);
   const [showInfoModal, setShowInfoModal] = useState(false);
+
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const toggleLeftSideDrawer = (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (event.type === 'keydown' && (
+        (event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift'))
+      {
+        return;
+      }
+      setDrawerOpen(!drawerOpen);
+  };
 
   return (
     <div className="App">
       <NavBar />
       <InfoModal showInfoModal={showInfoModal} setShowInfoModal={setShowInfoModal} />
       <PhotoViewer showImages={showImages} />
+
+      <LeftSideDrawer
+        drawerOpen={drawerOpen}
+        toggleLeftSideDrawer={toggleLeftSideDrawer}
+      />
 
       <VRCanvas>
         <DefaultXRControllers />
@@ -33,6 +50,10 @@ function App() {
           <Space />
         </Suspense>
       </VRCanvas>
+
+      <div className={`buttons-container buttons-container--left`}>
+        <Menu className="pointer" style={{ color: "white", margin: "0 4px" }} onClick={(event) => {toggleLeftSideDrawer(event)}}/>
+      </div>
 
       <div className="buttons-container">
         <InfoOutlined className="pointer" style={{ color: "white", margin: "0 4px" }} onClick={() => {setShowInfoModal(!showInfoModal)}}/>
---
+++ src/components/LeftSideDrawer.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+export default function LeftSideDrawer({drawerOpen, toggleLeftSideDrawer}: {drawerOpen: any, toggleLeftSideDrawer: any}) {
+
+  return (
+    <Drawer
+      open={drawerOpen}
+    >
+      <Box
+        component="div"
+        sx={{ width: 250 }}
+        role="presentation"
+        onClick={(event: any) => {toggleLeftSideDrawer(event)}}
+        onKeyDown={(event: any) => {toggleLeftSideDrawer(event)}}
+      >
+        <List>
+          {['Home', 'Room1', 'Room2'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['Photos', 'Info', 'VR', 'CLOSE BUTTON', 'BOOK THIS ROOM'].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  )
+}
