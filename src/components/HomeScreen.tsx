46ad05052773ed1a41c94399ce1cd936bae83cc5
Timothy Coleman <timhc22@gmail.com>
2022-07-12 04:50:15 +0100

    Remove enter button]

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -5,9 +5,9 @@ export default function HomeScreen({toggleLeftSideDrawer}: {toggleLeftSideDrawer
 
   return (
     <>
-      <div className="home-screen-cover">
+      <div className="home-screen-cover" onClick={(event:any) => {toggleLeftSideDrawer(event)}}>
         {/*<div className="home-screen-button-container">*/}
-          <Button size='large' className="home-screen-button" variant='contained' onClick={(event:any) => {toggleLeftSideDrawer(event)}}>Enter</Button>
+        {/*  <Button size='large' className="home-screen-button" variant='contained' onClick={(event:any) => {toggleLeftSideDrawer(event)}}>Enter</Button>*/}
         {/*</div>*/}
       </div>
       <div className="home-screen-image" style={{backgroundImage: `url(${process.env.REACT_APP_ASSETS_URL}/ark.jpeg)`}}/>
