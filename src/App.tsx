05e30c35704136adbdb9f3fc7dac8be68f5f7116
Timothy Coleman <timhc22@gmail.com>
2022-09-15 19:23:32 +0100

    Move extra controls out of app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,53 +16,14 @@ import Boudoir from "./components/Boudoir";
 import Gallery from "./components/Gallery";
 import MiddleFloor from "./components/MiddleFloor";
 import Dining from "./components/Dining";
-import GridOnIcon from '@mui/icons-material/GridOn';
 // import BookingModal from "./components/BookingModal";
-const initialHelperText = '↺ or ⇉ Model';
 
 function App() {
   const [showImages, setShowImages] = useState(false);
   const [showInfoModal, setShowInfoModal] = useState(false);
   const [showMenuModal, setShowMenuModal] = useState(false);
   const [showBookingModal, setShowBookingModal] = useState(false);
-  const [gridOn, setGridOn] = useState(false);
-  const [zoomOn, setZoomOn] = useState(false);
-  const [helperText, setHelperText] = useState(initialHelperText);
-
   const [drawerOpen, setDrawerOpen] = React.useState(false);
-
-  const showHelperTextMessage = () => {
-    let helperTextAlertMessage = 'Model can be rotated or panned: \n' +
-    'Controls vary depending on your device.\n\n' +
-    'Panning is usually two finger click and drag\n' +
-    'Or holding Command/Ctrl and then clicking and dragging.'
-    if(zoomOn) {
-      helperTextAlertMessage = helperTextAlertMessage + '\n\nZoom is usually pinch or scroll with 2 fingers.'
-    }
-
-    alert(helperTextAlertMessage);
-  };
-
-  const toggleGridOn = () => {
-    setGridOn(!gridOn);
-
-    if (!gridOn) {
-      setHelperText('1 Square is approx 1m²');
-    } else {
-      setHelperText(initialHelperText);
-    }
-  };
-
-  const toggleZoomOn = () => {
-    setZoomOn(!zoomOn);
-
-    if (!zoomOn) {
-      alert('PLEASE BE AWARE\nThis model is a reduced quality scan\nThis will be more noticeable when zooming!')
-      setHelperText(`⚲ or ${initialHelperText}`);
-    } else {
-      setHelperText(initialHelperText);
-    }
-  }
 
   const toggleLeftSideDrawer = (event: React.KeyboardEvent | React.MouseEvent) => {
       if (event.type === 'keydown' && (
@@ -112,35 +73,35 @@ function App() {
           key={'dining'}
           path="/dining"
           element={
-            <SpaceOne zoomOn={zoomOn} gridOn={gridOn} cameraPosition={[5,5,5]} space={<Dining />}/>
+            <SpaceOne cameraPosition={[5,5,5]} space={<Dining />}/>
           }
         />
         <Route
           key={'top-floor'}
           path="/top-floor"
           element={
-            <SpaceOne zoomOn={zoomOn} gridOn={gridOn} cameraPosition={[9,9,9]} space={<TopFloor />}/>
+            <SpaceOne cameraPosition={[9,9,9]} space={<TopFloor />}/>
           }
         />
         <Route
           key={'middle-floor'}
           path="/middle-floor"
           element={
-            <SpaceOne zoomOn={zoomOn} gridOn={gridOn} cameraPosition={[5,5,5]} space={<MiddleFloor/>}/>
+            <SpaceOne cameraPosition={[5,5,5]} space={<MiddleFloor/>}/>
           }
         />
         <Route
           key={'boudoir'}
           path="/boudoir"
           element={
-            <SpaceOne zoomOn={zoomOn} gridOn={gridOn} cameraPosition={[5,5,5]} space={<Boudoir/>}/>
+            <SpaceOne cameraPosition={[5,5,5]} space={<Boudoir/>}/>
           }
         />
         <Route
           key={'gallery'}
           path="/gallery"
           element={
-            <SpaceOne zoomOn={zoomOn} gridOn={gridOn} cameraPosition={[5,5,5]} space={<Gallery/>}/>
+            <SpaceOne cameraPosition={[5,5,5]} space={<Gallery/>}/>
           }
         />
 
@@ -156,18 +117,6 @@ function App() {
 
       <div className={`buttons-container buttons-container--left`}>
         <Menu className="pointer" style={{ color: "white", margin: "0 4px" }} onClick={(event) => {toggleLeftSideDrawer(event)}}/>
-        <p className='helperText' onClick={() => {showHelperTextMessage()}}>{helperText}</p>
-      </div>
-
-      <div className={`buttons-container buttons-container--mid-right`}>
-        <GridOnIcon className="pointer" style={{ color: "white", margin: "4px 4px" }} onClick={() => {toggleGridOn()}}/>
-
-        { zoomOn && (
-          <ZoomIn className="pointer" style={{ color: "white", margin: "4px 4px" }} onClick={() => {toggleZoomOn()}}/>
-        )}
-        { !zoomOn && (
-          <ZoomOut className="pointer" style={{ color: "white", margin: "4px 4px" }} onClick={() => {toggleZoomOn()}}/>
-        )}
       </div>
 
       <div className="buttons-container">
