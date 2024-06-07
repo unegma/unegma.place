70950fabe5fd3a9e44258e8e0f85ef143d3a7682
Tim <tim@unegma.com>
2024-06-07 16:15:50 +0100

    Add video component and favicons

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -6,7 +6,7 @@ import * as React from "react";
 export default function VideoComponent ({src}: any) {
 
   return (
-    <video width="100%" height="auto" preload="auto" autoPlay muted>
+    <video width="100%" height="auto" preload="auto" autoPlay muted loop>
       <source src={src} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
